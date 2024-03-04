74677ae722760d3b7c03902850d0befe86e0111b
Johnathon Lincoln <162115361+sendaitibet@users.noreply.github.com>
2024-03-04 12:07:09 +0200

    Create cods.cs

---
+++ cods.cs
@@ -0,0 +1,27 @@
+  static void Main()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        var folder = System.IO.Directory.CreateDirectory("output");
+        foreach (var file in folder.GetFiles()) file.Delete();
+
+        Random random = new();
+        XDocument xdoc = XDocument.Load("samples.xml");
+
+        foreach (XElement xelem in xdoc.Root.Elements("overlapping", "simpletiled"))
+        {
+
+           for (int i = 0; i < xelem.Get("screenshots", 2); i++)
+            {
+                for (int k = 0; k < 10; k++)
+                {
+                    Console.Write("> ");
+                    int seed = random.Next();
+                    bool success = model.Run(seed, xelem.Get("limit", -1));
+                    if (success)
+                    {
+                        Console.WriteLine("DONE");
+                        model.Save($"output/{name} {seed}.png");
+                        if (model is SimpleTiledModel stmodel && xelem.Get("textOutput", false))
+                            System.IO.File.WriteAllText($"output/{name} {seed}.txt", stmodel.TextOutput());
+                        break;
+                    }
